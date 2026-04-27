34a8acf37406db0d20055d3dc056a7c3797dbae7
piscoiandrei <piscoiandrei27@gmail.com>
2026-04-27 17:27:24 +0300

    start up fade

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,21 +6,22 @@ export default {
   ],
   theme: {
     extend: {
-      animation: {
-        'fade-in': 'fadeIn 1s ease-out',
-        'zoom-in': 'zoomIn 1s ease-out',
-      },
       keyframes: {
         fadeIn: {
           '0%': { opacity: '0' },
           '100%': { opacity: '1' },
         },
         zoomIn: {
-          '0%': { transform: 'scale(0.95)' },
-          '100%': { transform: 'scale(1)' },
+          '0%': { transform: 'scale(0.95)', opacity: '0' },
+          '100%': { transform: 'scale(1)', opacity: '1' },
         }
+      },
+      animation: {
+        'fade-in': 'fadeIn 1.5s ease-out',
+        'zoom-in': 'zoomIn 1.5s ease-out',
       }
     },
   },
   plugins: [],
 }
+
